2d8efd56c1b3e7cdb32231b03c29863ae5f66c73
Lee Spottiswood <lee.spottiswood@ukfast.co.uk>
2019-04-02 12:30:26 +0100

    fix tests

--- cmd/pss_test.go
+++ cmd/pss_test.go
@@ -38,24 +38,6 @@ func TestOutputPSSRequests_GetFieldData(t *testing.T) {
 		assert.True(t, data[0].Exists("id"))
 		assert.Equal(t, "123", data[0].Get("id").Value)
 	})
-
-	t.Run("NonNilAssignee_ExpectedAssignee", func(t *testing.T) {
-		o := OutputPSSRequests{
-			Requests: []pss.Request{
-				pss.Request{
-					Assignee: &pss.SupportUser{
-						Name: "test a",
-					},
-				},
-			},
-		}
-
-		data, err := o.GetFieldData()
-
-		assert.Nil(t, err)
-		assert.True(t, data[0].Exists("assignee_name"))
-		assert.Equal(t, "test a", data[0].Get("assignee_name").Value)
-	})
 }
 
 func TestOutputPSSRequestReplies_GetData_ExpectedData(t *testing.T) {
