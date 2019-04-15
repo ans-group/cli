40f36104feda0cc29e571f463e72cf578d0c985e
Lee Spottiswood <lee.spottiswood@ukfast.co.uk>
2019-04-15 10:52:40 +0100

    add domain CDN enable/disable commands

--- cmd/ddosx_domain_cdn.go
+++ cmd/ddosx_domain_cdn.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+	"github.com/ukfast/sdk-go/pkg/service/ddosx"
+)
 
 func ddosxDomainCDNRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -8,8 +13,94 @@ func ddosxDomainCDNRootCmd() *cobra.Command {
 		Short: "sub-commands relating to domain CDN",
 	}
 
+	// Child commands
+	cmd.AddCommand(ddosxDomainCDNEnableCmd())
+	cmd.AddCommand(ddosxDomainCDNDisableCmd())
+
 	// Child root commands
 	cmd.AddCommand(ddosxDomainCDNRuleRootCmd())
 
 	return cmd
+}
+
+func ddosxDomainCDNEnableCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "enable <domain: name>...",
+		Short:   "Enables CDN for a domain",
+		Long:    "This command enables CDN for one or more domains",
+		Example: "ukfast ddosx domain cdn enable example.com",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Missing domain")
+			}
+
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			ddosxDomainCDNEnable(getClient().DDoSXService(), cmd, args)
+		},
+	}
+}
+
+func ddosxDomainCDNEnable(service ddosx.DDoSXService, cmd *cobra.Command, args []string) {
+	var domains []ddosx.Domain
+
+	for _, arg := range args {
+		err := service.AddDomainCDNConfiguration(arg)
+		if err != nil {
+			OutputWithErrorLevelf("Error enabling CDN for domain [%s]: %s", arg, err.Error())
+			continue
+		}
+
+		domain, err := service.GetDomain(arg)
+		if err != nil {
+			OutputWithErrorLevelf("Error retrieving updated domain [%s]: %s", arg, err)
+			continue
+		}
+
+		domains = append(domains, domain)
+	}
+
+	outputDDoSXDomains(domains)
+}
+
+func ddosxDomainCDNDisableCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "disable <domain: name>...",
+		Short:   "Disables CDN for a domain",
+		Long:    "This command disables CDN for one or more domains",
+		Example: "ukfast ddosx domain cdn disable example.com",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Missing domain")
+			}
+
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			ddosxDomainCDNDisable(getClient().DDoSXService(), cmd, args)
+		},
+	}
+}
+
+func ddosxDomainCDNDisable(service ddosx.DDoSXService, cmd *cobra.Command, args []string) {
+	var domains []ddosx.Domain
+
+	for _, arg := range args {
+		err := service.DeleteDomainCDNConfiguration(arg)
+		if err != nil {
+			OutputWithErrorLevelf("Error disabling CDN for domain [%s]: %s", arg, err.Error())
+			continue
+		}
+
+		domain, err := service.GetDomain(arg)
+		if err != nil {
+			OutputWithErrorLevelf("Error retrieving updated domain [%s]: %s", arg, err)
+			continue
+		}
+
+		domains = append(domains, domain)
+	}
+
+	outputDDoSXDomains(domains)
 }
---
+++ cmd/ddosx_domain_cdn_test.go
@@ -0,0 +1,125 @@
+package cmd
+
+import (
+	"errors"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"github.com/ukfast/cli/test/mocks"
+	"github.com/ukfast/cli/test/test_output"
+	"github.com/ukfast/sdk-go/pkg/service/ddosx"
+)
+
+func Test_ddosxDomainCDNEnableCmd_Args(t *testing.T) {
+	t.Run("ValidArgs_NoError", func(t *testing.T) {
+		err := ddosxDomainCDNEnableCmd().Args(nil, []string{"testdomain1.co.uk"})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("MissingDomain_Error", func(t *testing.T) {
+		err := ddosxDomainCDNEnableCmd().Args(nil, []string{})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "Missing domain", err.Error())
+	})
+}
+
+func Test_ddosxDomainCDNEnable(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().AddDomainCDNConfiguration("testdomain1.co.uk").Return(nil)
+		service.EXPECT().GetDomain("testdomain1.co.uk").Return(ddosx.Domain{}, nil)
+
+		ddosxDomainCDNEnable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+	})
+
+	t.Run("AddDomainCDNConfiguration_OutputsError", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().AddDomainCDNConfiguration("testdomain1.co.uk").Return(errors.New("test error"))
+
+		test_output.AssertErrorOutput(t, "Error enabling CDN for domain [testdomain1.co.uk]: test error\n", func() {
+			ddosxDomainCDNEnable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+		})
+	})
+
+	t.Run("GetDomainError_OutputsError", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().AddDomainCDNConfiguration("testdomain1.co.uk").Return(nil)
+		service.EXPECT().GetDomain("testdomain1.co.uk").Return(ddosx.Domain{}, errors.New("test error"))
+
+		test_output.AssertErrorOutput(t, "Error retrieving updated domain [testdomain1.co.uk]: test error\n", func() {
+			ddosxDomainCDNEnable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+		})
+	})
+}
+
+func Test_ddosxDomainCDNDisableCmd_Args(t *testing.T) {
+	t.Run("ValidArgs_NoError", func(t *testing.T) {
+		err := ddosxDomainCDNDisableCmd().Args(nil, []string{"testdomain1.co.uk"})
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("MissingDomain_Error", func(t *testing.T) {
+		err := ddosxDomainCDNDisableCmd().Args(nil, []string{})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "Missing domain", err.Error())
+	})
+}
+
+func Test_ddosxDomainCDNDisable(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().DeleteDomainCDNConfiguration("testdomain1.co.uk").Return(nil)
+		service.EXPECT().GetDomain("testdomain1.co.uk").Return(ddosx.Domain{}, nil)
+
+		ddosxDomainCDNDisable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+	})
+
+	t.Run("DeleteDomainCDNConfiguration_OutputsError", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().DeleteDomainCDNConfiguration("testdomain1.co.uk").Return(errors.New("test error"))
+
+		test_output.AssertErrorOutput(t, "Error disabling CDN for domain [testdomain1.co.uk]: test error\n", func() {
+			ddosxDomainCDNDisable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+		})
+	})
+
+	t.Run("GetDomainError_OutputsError", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		service := mocks.NewMockDDoSXService(mockCtrl)
+
+		service.EXPECT().DeleteDomainCDNConfiguration("testdomain1.co.uk").Return(nil)
+		service.EXPECT().GetDomain("testdomain1.co.uk").Return(ddosx.Domain{}, errors.New("test error"))
+
+		test_output.AssertErrorOutput(t, "Error retrieving updated domain [testdomain1.co.uk]: test error\n", func() {
+			ddosxDomainCDNDisable(service, &cobra.Command{}, []string{"testdomain1.co.uk"})
+		})
+	})
+}
