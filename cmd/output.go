29b3493ac1190eb53015ec4aa26ee85deb92fd56
Lee Spottiswood <lee.spottiswood@ukfast.co.uk>
2019-07-29 14:17:42 +0100

    add support for generating bash completion

---
+++ cmd/output.go
@@ -0,0 +1,131 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ukfast/cli/internal/pkg/output"
+)
+
+var errorLevel int
+
+// OutputWithCustomErrorLevel is a wrapper for OutputError, which sets global
+// var errorLevel with provided level
+func OutputWithCustomErrorLevel(level int, str string) {
+	output.Error(str)
+	errorLevel = level
+}
+
+// OutputWithCustomErrorLevelf is a wrapper for OutputWithCustomErrorLevel, which sets global
+// var errorLevel with provided level
+func OutputWithCustomErrorLevelf(level int, format string, a ...interface{}) {
+	OutputWithCustomErrorLevel(level, fmt.Sprintf(format, a...))
+}
+
+// OutputWithErrorLevelf is a wrapper for OutputWithCustomErrorLevelf, which sets global
+// var errorLevel to 1
+func OutputWithErrorLevelf(format string, a ...interface{}) {
+	OutputWithCustomErrorLevelf(1, format, a...)
+}
+
+// OutputWithErrorLevel is a wrapper for OutputWithCustomErrorLevel, which sets global
+// var errorLevel to 1
+func OutputWithErrorLevel(str string) {
+	OutputWithCustomErrorLevel(1, str)
+}
+
+type OutputHandlerProvider interface {
+	GetData() interface{}
+	GetFieldData() ([]*output.OrderedFields, error)
+}
+
+type UnsupportedFormatHandler func() error
+
+type OutputHandler struct {
+	Format                   string
+	Provider                 OutputHandlerProvider
+	Properties               []string
+	Template                 string
+	SupportedFormats         []string
+	UnsupportedFormatHandler UnsupportedFormatHandler
+}
+
+func NewOutputHandler(out OutputHandlerProvider, format string) *OutputHandler {
+	if format == "" {
+		format = "table"
+	}
+
+	return &OutputHandler{
+		Provider: out,
+		Format:   format,
+	}
+}
+
+// Handle calls the relevant OutputProvider data retrieval methods for given value
+// in struct property 'Format'
+func (o *OutputHandler) Handle() error {
+	if !o.supportedFormat() {
+		if o.UnsupportedFormatHandler != nil {
+			return o.UnsupportedFormatHandler()
+		}
+
+		return fmt.Errorf("Unsupported output format [%s], supported formats: %s", o.Format, strings.Join(o.SupportedFormats, ", "))
+	}
+
+	switch o.Format {
+	case "json":
+		return output.JSON(o.Provider.GetData())
+	case "template":
+		return output.Template(o.Template, o.Provider.GetData())
+	case "value":
+		d, err := o.Provider.GetFieldData()
+		if err != nil {
+			return err
+		}
+		return output.Value(o.Properties, d)
+	case "csv":
+		d, err := o.Provider.GetFieldData()
+		if err != nil {
+			return err
+		}
+		return output.CSV(o.Properties, d)
+	default:
+		output.Errorf("Invalid output format [%s], defaulting to 'table'", o.Format)
+		fallthrough
+	case "table":
+		d, err := o.Provider.GetFieldData()
+		if err != nil {
+			return err
+		}
+		return output.Table(o.Properties, d)
+	}
+}
+
+func (o *OutputHandler) supportedFormat() bool {
+	if o.SupportedFormats == nil {
+		return true
+	}
+
+	for _, supportedFormat := range o.SupportedFormats {
+		if strings.ToLower(supportedFormat) == o.Format {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Output calls the relevant OutputProvider data retrieval methods for given value
+// in global variable 'flagFormat'
+func Output(out OutputHandlerProvider) error {
+	handler := NewOutputHandler(out, flagFormat)
+	handler.Properties = flagProperty
+	handler.Template = flagOutputTemplate
+
+	return handler.Handle()
+}
+
+func Exit() {
+	os.Exit(errorLevel)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -2,11 +2,8 @@ package cmd
 
 import (
 	"crypto/tls"
-	"errors"
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -14,7 +11,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/ukfast/cli/internal/pkg/build"
-	"github.com/ukfast/cli/internal/pkg/helper"
 	"github.com/ukfast/cli/internal/pkg/output"
 	apiclient "github.com/ukfast/sdk-go/pkg/client"
 	"github.com/ukfast/sdk-go/pkg/connection"
@@ -27,7 +23,6 @@ var flagOutputTemplate string
 var flagSort string
 var flagProperty []string
 var flagFilter []string
-var errorLevel int
 var appFilesystem afero.Fs
 
 // rootCmd represents the base command when called without any subcommands
@@ -35,6 +30,24 @@ var rootCmd = &cobra.Command{
 	Use:     "ukfast",
 	Short:   "Utility for manipulating UKFast services",
 	Version: "UNKNOWN",
+}
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion scripts",
+	Long: `To load completion run
+
+. <(ukfast completion)
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# ~/.bashrc or ~/.profile
+. <(ukfast completion)
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rootCmd.GenBashCompletion(os.Stdout)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -46,9 +59,7 @@ func Execute(build build.BuildInfo) {
 		output.Fatal(err.Error())
 	}
 
-	if errorLevel > 0 {
-		os.Exit(errorLevel)
-	}
+	Exit()
 }
 
 func init() {
@@ -61,6 +72,9 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&flagSort, "sort", "", "output sorting, e.g. 'name', 'name:asc', 'name:desc'")
 	rootCmd.PersistentFlags().StringSliceVar(&flagProperty, "property", []string{}, "property to output (used with several formats), can be repeated")
 	rootCmd.PersistentFlags().StringArrayVar(&flagFilter, "filter", []string{}, "filter for list commands, can be repeated, e.g. 'property=somevalue', 'property:gt=3', 'property=valu*'")
+
+	// Child commands
+	rootCmd.AddCommand(completionCmd)
 
 	// Child root commands
 	rootCmd.AddCommand(safednsRootCmd())
@@ -141,173 +155,4 @@ func getClient() apiclient.Client {
 	}
 
 	return apiclient.NewClient(conn)
-}
-
-// OutputWithCustomErrorLevel is a wrapper for OutputError, which sets global
-// var errorLevel with provided level
-func OutputWithCustomErrorLevel(level int, str string) {
-	output.Error(str)
-	errorLevel = level
-}
-
-// OutputWithCustomErrorLevelf is a wrapper for OutputWithCustomErrorLevel, which sets global
-// var errorLevel with provided level
-func OutputWithCustomErrorLevelf(level int, format string, a ...interface{}) {
-	OutputWithCustomErrorLevel(level, fmt.Sprintf(format, a...))
-}
-
-// OutputWithErrorLevelf is a wrapper for OutputWithCustomErrorLevelf, which sets global
-// var errorLevel to 1
-func OutputWithErrorLevelf(format string, a ...interface{}) {
-	OutputWithCustomErrorLevelf(1, format, a...)
-}
-
-// OutputWithErrorLevel is a wrapper for OutputWithCustomErrorLevel, which sets global
-// var errorLevel to 1
-func OutputWithErrorLevel(str string) {
-	OutputWithCustomErrorLevel(1, str)
-}
-
-type OutputHandlerProvider interface {
-	GetData() interface{}
-	GetFieldData() ([]*output.OrderedFields, error)
-}
-
-type UnsupportedFormatHandler func() error
-
-type OutputHandler struct {
-	Format                   string
-	Provider                 OutputHandlerProvider
-	Properties               []string
-	Template                 string
-	SupportedFormats         []string
-	UnsupportedFormatHandler UnsupportedFormatHandler
-}
-
-func NewOutputHandler(out OutputHandlerProvider, format string) *OutputHandler {
-	if format == "" {
-		format = "table"
-	}
-
-	return &OutputHandler{
-		Provider: out,
-		Format:   format,
-	}
-}
-
-// Handle calls the relevant OutputProvider data retrieval methods for given value
-// in struct property 'Format'
-func (o *OutputHandler) Handle() error {
-	if !o.supportedFormat() {
-		if o.UnsupportedFormatHandler != nil {
-			return o.UnsupportedFormatHandler()
-		}
-
-		return fmt.Errorf("Unsupported output format [%s], supported formats: %s", o.Format, strings.Join(o.SupportedFormats, ", "))
-	}
-
-	switch o.Format {
-	case "json":
-		return output.JSON(o.Provider.GetData())
-	case "template":
-		return output.Template(o.Template, o.Provider.GetData())
-	case "value":
-		d, err := o.Provider.GetFieldData()
-		if err != nil {
-			return err
-		}
-		return output.Value(o.Properties, d)
-	case "csv":
-		d, err := o.Provider.GetFieldData()
-		if err != nil {
-			return err
-		}
-		return output.CSV(o.Properties, d)
-	default:
-		output.Errorf("Invalid output format [%s], defaulting to 'table'", o.Format)
-		fallthrough
-	case "table":
-		d, err := o.Provider.GetFieldData()
-		if err != nil {
-			return err
-		}
-		return output.Table(o.Properties, d)
-	}
-}
-
-func (o *OutputHandler) supportedFormat() bool {
-	if o.SupportedFormats == nil {
-		return true
-	}
-
-	for _, supportedFormat := range o.SupportedFormats {
-		if strings.ToLower(supportedFormat) == o.Format {
-			return true
-		}
-	}
-
-	return false
-}
-
-// Output calls the relevant OutputProvider data retrieval methods for given value
-// in global variable 'flagFormat'
-func Output(out OutputHandlerProvider) error {
-	handler := NewOutputHandler(out, flagFormat)
-	handler.Properties = flagProperty
-	handler.Template = flagOutputTemplate
-
-	return handler.Handle()
-}
-
-type APIListParameters struct {
-	Filtering []connection.APIRequestFiltering
-	Sorting   connection.APIRequestSorting
-}
-
-func GetAPIRequestParametersFromFlags() (connection.APIRequestParameters, error) {
-	filtering, err := helper.GetFilteringArrayFromStringArrayFlag(flagFilter)
-	if err != nil {
-		return connection.APIRequestParameters{}, err
-	}
-
-	return connection.APIRequestParameters{
-		Sorting:   helper.GetSortingFromStringFlag(flagSort),
-		Filtering: filtering,
-		Pagination: connection.APIRequestPagination{
-			PerPage: viper.GetInt("api_pagination_perpage"),
-		},
-	}, nil
-}
-
-type WaitFunc func() (finished bool, err error)
-
-func WaitForCommand(f WaitFunc) error {
-	waitTimeout := viper.GetInt("command_wait_timeout_seconds")
-	if waitTimeout < 1 {
-		return errors.New("Invalid command_wait_timeout_seconds")
-	}
-	sleepTimeout := viper.GetInt("command_wait_sleep_seconds")
-	if sleepTimeout < 1 {
-		return errors.New("Invalid command_wait_sleep_seconds")
-	}
-
-	timeStart := time.Now()
-
-	for {
-		if time.Since(timeStart).Seconds() > float64(waitTimeout) {
-			return errors.New("Timed out waiting for command")
-		}
-
-		finished, err := f()
-		if err != nil {
-			return fmt.Errorf("Error waiting for command: %s", err)
-		}
-		if finished {
-			break
-		}
-
-		time.Sleep(time.Duration(sleepTimeout) * time.Second)
-	}
-
-	return nil
 }
